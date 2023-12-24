840c6e121fdceb082d3ea4b44c3ecf653379b43a
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-24 09:32:27 +0530

    Add files via upload

---
+++ IssueTrackerApp/DAL/Services/IssueTrackerService.cs
@@ -0,0 +1,47 @@
+﻿using IssueTrackerApp.DAL.Interrfaces;
+using IssueTrackerApp.DAL.Services.Repository;
+using IssueTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace IssueTrackerApp.DAL.Services
+{
+    public class IssueTrackerService : IIssueTrackerService
+    {
+        private readonly IIssueTrackerRepository _repository;
+
+        public IssueTrackerService(IIssueTrackerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Issue> CreateIssue(Issue issue)
+        {
+            return _repository.CreateIssue(issue);
+        }
+
+        public Task<bool> DeleteIssueById(long id)
+        {
+            return _repository.DeleteIssueById(id);
+        }
+
+        public List<Issue> GetAllIssues()
+        {
+            return _repository.GetAllIssues();
+        }
+
+        public Task<Issue> GetIssueById(long id)
+        {
+            return _repository.GetIssueById(id);
+        }
+
+        public Task<Issue> UpdateIssue(Issue model)
+        {
+            return _repository.UpdateIssue(model);
+        }
+    }
+}
---
+++ IssueTrackerApp/Models/Issue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IssueTrackerApp.Models
+{
+    public class Issue
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string AssignedTo { get; set; }
+        public bool IsOpen { get; set; }
+    }
+}
