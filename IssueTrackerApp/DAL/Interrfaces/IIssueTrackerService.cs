942e96a12f4a5fd660a8a14afeb5f0a1e73a1f69
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-20 09:50:30 +0530

    Add files via upload

---
+++ IssueTrackerApp/DAL/Interrfaces/IIssueTrackerService.cs
@@ -0,0 +1,18 @@
+﻿using IssueTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTrackerApp.DAL.Interrfaces
+{
+    public interface IIssueTrackerService
+    {
+        List<Issue> GetAllIssues();
+        Task<Issue> CreateIssue(Issue issue);
+        Task<Issue> GetIssueById(long id);
+        Task<bool> DeleteIssueById(long id);
+        Task<Issue> UpdateIssue(Issue model);
+    }
+}
---
+++ IssueTrackerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IssueTrackerApp.WebApiApplication" Language="C#" %>
